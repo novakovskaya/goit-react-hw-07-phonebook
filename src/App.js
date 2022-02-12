d94d0fd9fe79ede48a5c8be42b3ba6fd3651774c
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-02-12 16:47:23 +0200

    Added useEffect, loader and notification

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import Container from "./components/Container";
-import Section from "./components/Section";
-import ContactForm from "./components/ContactForm";
-import ContactList from "./components/ContactList";
-import Filter from "./components/Filter";
+import { useDispatch, useSelector } from 'react-redux';
+import { contactsSelectors, contactsOperations } from 'redux/contacts';
+import { useEffect } from 'react';
+import { TailSpin } from 'react-loader-spinner';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => (
-  <Container>
-    <Section title="Phonebook">
-      <ContactForm />
-    </Section>
+import { Container } from './components/Container';
+import { Section } from './components/Section';
+import { ContactForm } from './components/ContactForm';
+import { ContactList } from './components/ContactList';
+import { Filter } from './components/Filter';
 
-    <Section title="Contacts">
-      <Filter />
-      <ContactList />
-    </Section>
-  </Container>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(contactsSelectors.getIsLoading);
+  const isError = useSelector(contactsSelectors.getError);
+
+  useEffect(() => dispatch(contactsOperations.fetchContacts()), [dispatch]);
+
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+
+      <Section title="Contacts">
+        <Filter />
+        {isLoading && (
+          <TailSpin
+            heigth="80"
+            width="80"
+            color="#A5CED0"
+            ariaLabel="loading"
+          />
+        )}
+        {isError && toast.error('We were unable to load contacts!')}
+        <ContactList />
+      </Section>
+      <ToastContainer autoClose={1500} />
+    </Container>
+  );
+};
 
 export default App;
