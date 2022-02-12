e1c34526d57ea0359f36a7813ad0a4fad8111b28
Yulia Novakovskaya <yonovakovskaya@gmail.com>
2022-02-12 16:50:20 +0200

    Update redux-actions

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,15 +1,3 @@
-import { createAction } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+import { createAction } from '@reduxjs/toolkit';
 
-export const addContact = createAction("contacts/add", ({ name, number }) => ({
-  payload: {
-    id: nanoid(),
-    name,
-    number,
-  },
-}));
-
-export const deleteContact = createAction("contacts/delete");
-export const filterContact = createAction("contacts/filter");
-
-
+export const filterContacts = createAction('contacts/filter');
